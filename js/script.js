16b95a2a21197154c6e29a1698da973f1935a366
LAP-DELL\rober <robert_esparza@hotmail.com>
2019-02-07 01:06:55 -0600

    Version 1.0

---
+++ js/script.js
@@ -0,0 +1,15 @@
+$(function() {
+    $.each($('[data-toggle="popover"]'), function(i, val) {
+        var id = $(val).attr("id");
+        if (id != undefined) {
+            var html = $("." + id).html();
+            console.log(html)
+            $(val).popover({
+                'trigger': 'focus',
+                'placement': 'bottom',
+                'content': html,
+                'html': true
+            });
+        }
+    });
+})
